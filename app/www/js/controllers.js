d40359b40a47e0d08ab4f2465ba6277ca17d3c00
Ari Weiland <math1man@gmail.com>
2016-02-20 10:47:17 -0600

    got the server working added the loading spinning orange

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -1,44 +1,29 @@
 (function() {
 
+  const URL = 'https://macathon2016.appspot.com/_ah/api';
+
   var app = angular.module('bagels.controllers', []);
 
-  app.controller('IndexController', function($scope, $window, $state) {
+  app.controller('IndexController', function($scope, $window, $state, API) {
 
     $window.init = function() {
       if (!$state.is('home')) {
         $state.go('home');
       }
-    };
 
-    $scope.showSettings = function() {
-      // TODO settings eventually
+      gapi.client.load('events', 'v1', function() { // TODO
+        // initialize API variable
+        API.init(gapi.client.events.eventsApi);   // TODO
+
+        //API.getEvent();
+
+      }, URL);
     };
 
   });
 
-  app.controller('ActivityController', function($scope, $cordovaInAppBrowser, Activity) {
+  app.controller('HomeController', function($scope, $state, $interval, API, Activity, Loading) {
 
-    $scope.showDesc = false;
-
-    $scope.toggleDesc = function() {
-      $scope.showDesc = !$scope.showDesc;
-    };
-
-    $scope.goWebsite = function() {
-      $cordovaInAppBrowser.open(Activity.url, '_system')
-    };
-
-    $scope.activity = function(field) {
-      return Activity[field];
-    };
-
-
-    $scope.changeView = function(){
-      $state.go('settings')
-    };
-  });
-
-  app.controller('HomeController', function($scope, $interval) {
     var verbs = [
       'dance',
       'try something new',
@@ -73,7 +58,114 @@
 
     $scope.getVerb = function() {
       return verbs[index];
-    }
+    };
+
+    $scope.whatDo = function() {
+      Loading.is = true;
+      API.getEvent().then(function(resp) {
+        Activity.activity = resp.result;
+        Loading.is = false;
+      });
+    };
+
+  });
+
+  app.controller('ActivityController', function($scope, $cordovaInAppBrowser, $ionicPopup, API, Activity, Loading) {
+
+    $scope.showDesc = false;
+    $scope.toggleDesc = function() {
+      $scope.showDesc = !$scope.showDesc;
+    };
+
+    $scope.goWebsite = function() {
+      if (Activity.activity.url != null) {
+        $cordovaInAppBrowser.open(Activity.activity.url, '_system')
+      }
+    };
+
+    $scope.activity = function(field) {
+      return Activity.activity[field];
+    };
+
+    $scope.refresh = function() {
+      Loading.is = true;
+      API.getEvent().then(function(resp) {
+        Activity.activity = resp.result;
+        Loading.is = false;
+      });
+    };
+
+    $scope.isLoading = function() {
+      return Loading.is;
+    };
+
+    $scope.tooExpensive = function() {
+      var confirmPopup = $ionicPopup.confirm({
+        title: 'Too expensive',
+        template: 'This activity is too expensive for me.'
+      });
+
+      confirmPopup.then(function(res) {
+        if(res) {
+          Loading.is = true;
+          API.getEvent().then(function(resp) {
+            Activity.activity = resp.result;
+            Loading.is = false;
+          });
+        }
+      });
+    };
+
+    $scope.tooManyPeople = function() {
+      var confirmPopup = $ionicPopup.confirm({
+        title: 'Too many people',
+        template: 'We have too many people for this activity.'
+      });
+
+      confirmPopup.then(function(res) {
+        if(res) {
+          Loading.is = true;
+          API.getEvent().then(function(resp) {
+            Activity.activity = resp.result;
+            Loading.is = false;
+          });
+        }
+      });
+    };
+
+    $scope.tooLong = function() {
+      var confirmPopup = $ionicPopup.confirm({
+        title: 'Too long',
+        template: 'This activity will take too long.'
+      });
+
+      confirmPopup.then(function(res) {
+        if(res) {
+          Loading.is = true;
+          API.getEvent().then(function(resp) {
+            Activity.activity = resp.result;
+            Loading.is = false;
+          });
+        }
+      });
+    };
+
+    $scope.tooFarAway = function() {
+      var confirmPopup = $ionicPopup.confirm({
+        title: 'Too far away',
+        template: 'This activity is too far away.'
+      });
+
+      confirmPopup.then(function(res) {
+        if(res) {
+          Loading.is = true;
+          API.getEvent().then(function(resp) {
+            Activity.activity = resp.result;
+            Loading.is = false;
+          });
+        }
+      });
+    };
 
   });
 
--- app/www/js/services.js
+++ app/www/js/services.js
@@ -3,14 +3,15 @@
   var app = angular.module('bagels.services', []);
 
   app.service('Activity', function() {
-    this.name = 'Walker Art Center';
-    this.url = 'http://www.walkerart.org/';
-    this.cost = '$9-14 per person';
-    this.duration = 'Varies';
-    this.groupSize = 'Any';
-    this.distance = '8 miles';
-    this.description = 'The Walker Art Center is a multidisciplinary contemporary art center. ' +
-        'The Walker is considered one of America\'s premier museums for modern art.';
+    this.activity = {
+      name: '-',
+      url: '-',
+      cost: '-',
+      duration: '-',
+      groupSize: '-',
+      distance: '-',
+      description: '-'
+    };
   });
 
   app.service('Preferences', function() {
@@ -22,6 +23,24 @@
     this.sports = true;
     this.social = true;
     this.volunteering = true;
-  })
+  });
+
+  app.factory('API', function() {
+
+    var api = null;
+
+    return {
+      init: function(apiInit) {
+        api = apiInit;
+      },
+      getEvent: function() {
+        return api.getEvent({id: 0});
+      }
+    };
+  });
+
+  app.service('Loading', function() {
+    this.is = false;
+  });
 
 })();
